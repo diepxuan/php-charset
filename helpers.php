39878eee3fdc86bfec473c603ff04cfb3bb23eff
Trần Ngọc Đức <caothu91@gmail.com>
2024-05-09 11:33:46 +0700

    create function vn_convert_encoding

--- helpers.php
+++ helpers.php
@@ -1,3 +1,13 @@
 <?php
 
 require_one "vn_charset_conversion.php";
+
+if (!function_exists('vn_convert_encoding')) {
+    /**
+     * List packages.
+     */
+    function vn_convert_encoding($string): string
+    {
+        return new \Diepxuan\Charset\VnEcoding($string);
+    }
+}
